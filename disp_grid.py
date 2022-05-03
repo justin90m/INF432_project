38b84e81b2450af8001c53bbd96fc3a6ff411baf
justin90m <78233247+justin90m@users.noreply.github.com>
2022-05-03 12:36:02 +0200

    Update disp_grid.py

--- disp_grid.py
+++ disp_grid.py
@@ -39,9 +39,40 @@ def check_grid(grid):
         print("Couldn't generate a correct grid from the given grid")
         return
     return mod_grid
-
-
-
+#checks if the given grid is a correct final grid : correct grid with only 1s and 0s and replace '0' and '1' by 0 and 1
+def check_final_grid(grid)
+    for i in range(len(grid)):
+        if len(grid)!=len(grid[i]):
+            print("Fianle grid not valid : number of line must be equal to number of columns.")
+            print("ligne ", i, "has ", len(grid[i]), "elements while the grid has ", len(grid), "lines")
+            return
+    #Now we know that the nb_lines=nb_col
+    if len(grid)%2!=0:
+        print("The size of the final grid must be even")
+        return
+    #now the grid should be correct, so we just need to modify it
+    mod_grid = [[0 for x in range(len(grid[y]))] for y in range(len(grid))]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]==0 or grid[i][j]==1 or grid[i][j]==2:
+                mod_grid[i][j] = grid[i][j]
+            elif grid[i][j]=='1' or grid[i][j]=='0':
+                mod_grid[i][j]=int(grid[i][j])
+            elif grid[i][j]==None:
+                print("Final grid not commplete : final_grid[", i, "][", j, "]=None")
+                return
+            else:
+                print("incorrect element in the final grid at line ", i, "column ", j, " : ", grid[i][j])
+    #We check if the new grid is correct too
+    for i in range(len(mod_grid)):
+        if len(mod_grid)!=len(mod_grid[i]):
+            print("Couldn't generate a correct grid from the final grid")
+            print("ligne ", i, "has ", len(mod_grid[i]), "elements while the grid has ", len(mod_grid), "lines")
+            return
+    if len(mod_grid)%2!=0:
+        print("Couldn't generate a correct grid from the final grid")
+        return
+    return mod_grid
 def set_px_py(grid_size):
     if grid_size==16:
         return (-8,-5)
@@ -85,6 +116,8 @@ def disp_original_grid(grid):
     win.fill(WHITE)
     #grid parameters
     grid = check_grid(grid)
+    if grid==None:
+        return
     grid_size=len(grid)
     size_squares = GRID_HEIGHT // grid_size
     num_size=set_size(grid_size)
@@ -99,6 +132,43 @@ def disp_original_grid(grid):
         for j in range(0,grid_size):
             if grid[i][j]==0 or grid[i][j]==1:
                 value = font_numbers.render(str(grid[i][j]), True, original_elt_colour)
+                win.blit(value, (j*size_squares+d+15+px_grid, i*size_squares+d+py_grid))
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+def disp_final_grid(original_grid,final_grid):
+    #Window parameters
+    pygame.init()
+    win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("TAKUZU")
+    CLOCK = pygame.time.Clock()
+    win.fill(WHITE)
+    #grid parameters
+    original_grid=check_grid(original_grid)
+    final_grid = check_final_grid(final_grid)
+    if final_grid==None:
+        return
+    grid_size=len(final_grid)
+    size_squares = GRID_HEIGHT // grid_size
+    num_size=set_size(grid_size)
+    font_numbers = pygame.font.SysFont('Comic Sans Ms', num_size)
+    d = (WINDOW_WIDTH-GRID_WIDTH) // 2
+    (px_grid,py_grid)=set_px_py(grid_size)
+    for i in range(grid_size+1):
+        pygame.draw.line(win, (0,0,0), (d+size_squares*i, d), (d+size_squares*i,d+size_squares*grid_size), 2)
+        pygame.draw.line(win, (0,0,0), (d, d+size_squares*i), (d+size_squares*grid_size, d+size_squares*i), 2)
+    #i=ordonnée, j=abcisse
+    for i in range(0, grid_size):
+        for j in range(0,grid_size):
+            if original_grid[i][j]==0 or original_grid[i][j]==1:
+                value = font_numbers.render(str(original_grid[i][j]), True, original_elt_colour)
+                win.blit(value, (j*size_squares+d+15+px_grid, i*size_squares+d+py_grid))
+            elif final_grid[i][j]==0 or final_grid[i][j]==1:
+                value = font_numbers.render(str(final_grid[i][j]), True, BLACK)
                 win.blit(value, (j*size_squares+d+15+px_grid, i*size_squares+d+py_grid))
     pygame.display.update()
     while True:
