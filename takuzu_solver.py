2639415ac01d6e4c1f7a1d25814d92b22e83a5f5
FarahSeifeddine <74819595+FarahSeifeddine@users.noreply.github.com>
2022-05-12 21:28:26 +0200

    Update takuzu_solver.py

--- takuzu_solver.py
+++ takuzu_solver.py
@@ -2,7 +2,8 @@ import itertools
 import subprocess
 import os
 
-N = int(input("length of the grid: "))
+##N = int(input("length of the grid: "))
+N = 4
 # add files for corresponding N values
 # add test because n has to be even
 
@@ -13,7 +14,7 @@ def grid_from_file(file_name):
     grid=f.readlines()
     #lignes = [[8]; [0010--10]; [0000000] ... ]
     # n = lignes[0]
-    n=grid[0];
+    n=grid[0]
     # test if n = length of list - 1
     if int(n) != (len(grid)-1) :
         print("erreur d'entree, BYE")
@@ -23,7 +24,7 @@ def grid_from_file(file_name):
     for i in range(len(grid)):
     #create list l
         l = []
-        ligne = grid[i];
+        ligne = grid[i]
     #if elem type is char add each char to l
         for j in range(len(ligne)):
             case = ligne[j]
@@ -37,7 +38,7 @@ def grid_from_file(file_name):
             elif case == '-':
                 l.append(None)                
         #and then lignes[indice] = l
-        grid[i] = l;
+        grid[i] = l
     f.close()   
     return grid
 
@@ -95,8 +96,8 @@ def constraints_from_grid(grid):
     
     for j in range(1,n+1):
         for i in range(1,n-1):
-            constraints+= [f"x_{i}_{j}",f"x_{i+1}_{j}",f"x_{i+2}_{j}"]
-            constraints+= [f"-x_{i}_{j}",f"-x_{i+1}_{j}",f"-x_{i+2}_{j}"]
+            constraints+= [[f"x_{i}_{j}",f"x_{i+1}_{j}",f"x_{i+2}_{j}"]]
+            constraints+= [[f"-x_{i}_{j}",f"-x_{i+1}_{j}",f"-x_{i+2}_{j}"]]
 
 
     #(3) There can be no identical rows or columns.
@@ -106,7 +107,7 @@ def constraints_from_grid(grid):
             for j2 in range(1,n+1):
                 if j1 != j2:
                     OU+= [f"z_{i}_{j1}_{i}_{j2}"]
-        constraints+= OU
+        constraints+= [OU]
                           
     for j in range(1,n+1):
         OU=[]
@@ -114,7 +115,7 @@ def constraints_from_grid(grid):
             for i2 in range(1,n+1):
                 if i1 != i2:
                     OU+= [f"z_{i1}_{j}_{i2}_{j}"]
-    constraints+= OU
+        constraints+= [OU]
 
     #(4) Each puzzle begins with several squares in the grid already filled.
     for i in range(n):
@@ -139,7 +140,7 @@ def dimacs_string_from_constraints(constraints):
         for var_name in constraint:
             if var_name not in L:
                 L.append(var_name) 
-            
+
     sorted_L = sorted(L)
     d = dict()      # keys of d = variable names and their values are indexes
     d2 = dict()     # keys of d2 = indexes and their values are variable names
@@ -148,80 +149,43 @@ def dimacs_string_from_constraints(constraints):
         d[v] = i+1
         d2[i+1] = v
 
-    print(d)
-    print()
-    print(d2)
-
     for constraint in constraints:
         for var_name in constraint:
             index = d[var_name]
             res+= str(index) + " " 
         res+= "0\n"
 
-    return res, d, d2
+    return res, d2
 
 
-def solution_from_dimacs_string(dimacs_str):
+def solution_from_dimacs_string(dimacs_str, d2):
     # write the dimacs into a file
     with open("constraints.dimacs.tmp", 'w') as f:
         f.write(dimacs_str)
     # run a solver (here minisat)
-    subprocess.run(["minisat", "constraints.dimacs.tmp", "out.tmp"], shell=True, check=True)
+    subprocess.run(["minisat", "constraints.dimacs.tmp", "out.tmp"])
     with open("out.tmp", 'r') as output:
         solution_as_str = output.read().splitlines()
+    os.remove("constraints.dimacs.tmp")
+    os.remove("out.tmp")
 
-    print("hi")
-    print(solution_as_str)
-##    
-##    os.remove("constraints.dimacs.tmp")
-##    os.remove("out.tmp")
-##
-##    if solution_as_str[0] == 'UNSAT':
-##        return "UNSAT"
+    if solution_as_str[0] == 'UNSAT':
+        return "UNSAT"
     # else
-##    solution_as_str = {int(i) for i in solution_as_str[1].split()}
-##    solution_grid = [[None for _ in range(9)] for __ in range(9)]
-##    for i in range(9):
-##        for j in range(9):
-##            for k in range(1, 10):
-##                if 100 * i + 10 * j + k in solution_as_str:
-##                    solution_grid[i][j] = k
-##    return solution_grid
-    return None
+    solution_as_str = {int(i) for i in solution_as_str[1].split()}
+    solution_grid = [[0 for _ in range(N)] for __ in range(N)]
+    for nb in solution_as_str:
+        if nb >0:
+            var_name = d2[nb]
+            if 'z' not in var_name:
+                i = int(var_name.split('_')[-2])
+                j = int(var_name.split('_')[-1])
+            # if 'z' in var_name:
+            #     i_prime = int(var_name.split('_')[-4])
+            #     j_prime = int(var_name.split('_')[-3])
+                solution_grid[i-1][j-1] = 1
+    
+    return solution_grid
 
 
-##def solution_from_dimacs_string(dimacs_str):
-##    # write the dimacs into a file
-##    with open("constraints.dimacs.tmp", 'w') as f:
-##        f.write(dimacs_str)
-##    # run a solver (here minisat)
-##    subprocess.run(["minisat", "constraints.dimacs.tmp", "out.tmp"])
-##    with open("out.tmp", 'r') as output:
-##        solution_as_str = output.read().splitlines()
-##
-##    os.remove("constraints.dimacs.tmp")
-##    os.remove("out.tmp")
-##
-##    if solution_as_str[0] == 'UNSAT':
-##        return "UNSAT"
-##    # else
-##    solution_as_str = {int(i) for i in solution_as_str[1].split()}
-##    solution_grid = [[None for _ in range(9)] for __ in range(9)]
-##    for i in range(9):
-##        for j in range(9):
-##            for k in range(1, 10):
-##                if 100 * i + 10 * j + k in solution_as_str:
-##                    solution_grid[i][j] = k
-##    return solution_grid
 
-
-# MAIN
-
-grid = [[0, None, None,0],
-        [None,None,None,1],
-        [None, None, None,None],
-        [None,None,None,1]]
-constraints_from_grid(grid)
-res,d, d2 = dimacs_string_from_constraints([['z_4_3', 'z_4_4'],['-z_1_2']])
-print(res)
-solution_from_dimacs_string(res)
