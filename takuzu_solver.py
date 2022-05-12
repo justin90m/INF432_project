d0d955a1aee73a9099f8e419c6daf8c654d42260
FarahSeifeddine <74819595+FarahSeifeddine@users.noreply.github.com>
2022-05-12 23:16:12 +0200

    Update takuzu_solver.py

--- takuzu_solver.py
+++ takuzu_solver.py
@@ -128,37 +128,131 @@ def constraints_from_grid(grid):
     return constraints
 
 
+# def dimacs_string_from_constraints(constraints):
+#     """Numbering rule: index in list of var_name"""
+#     res = ""
+#     nb_var = N*N
+#     nb_clauses = len(constraints)
+#     res+= f"p cnf {nb_var} {nb_clauses}\n"
+
+#     L = []
+#     for constraint in constraints:
+#         for var_name in constraint:
+#             if var_name not in L:
+#                 L.append(var_name) 
+
+#     sorted_L = sorted(L)
+#     d = dict()      # keys of d = variable names and their values are indexes
+#     d2 = dict()     # keys of d2 = indexes and their values are variable names
+
+#     for i,v in enumerate(L):
+#         d[v] = i+1
+#         d2[i+1] = v
+
+#     for constraint in constraints:
+#         for var_name in constraint:
+#             index = d[var_name]
+#             res+= str(index) + " " 
+#         res+= "0\n"
+
+#     return res, d2
+
+
+# def solution_from_dimacs_string(dimacs_str, d2):
+#     # write the dimacs into a file
+#     with open("constraints.dimacs.tmp", 'w') as f:
+#         f.write(dimacs_str)
+#     # run a solver (here minisat)
+#     subprocess.run(["minisat", "constraints.dimacs.tmp", "out.tmp"])
+#     with open("out.tmp", 'r') as output:
+#         solution_as_str = output.read().splitlines()
+#     os.remove("constraints.dimacs.tmp")
+#     os.remove("out.tmp")
+
+#     if solution_as_str[0] == 'UNSAT':
+#         return "UNSAT"
+#     # else
+#     solution_as_str = {int(i) for i in solution_as_str[1].split()}
+#     # solution_grid = [[0 for _ in range(N)] for __ in range(N)]
+#     # for nb in solution_as_str:
+#     #     if nb >0:
+#     #         var_name = d2[nb]
+#     #         if 'z' not in var_name:
+#     #             i = int(var_name.split('_')[-2])
+#     #             j = int(var_name.split('_')[-1])
+#     #         # if 'z' in var_name:
+#     #         #     i_prime = int(var_name.split('_')[-4])
+#     #         #     j_prime = int(var_name.split('_')[-3])
+#     #             solution_grid[i-1][j-1] = 1
+#     solution_grid = [[None for _ in range(N)] for __ in range(N)]
+#     for nb in solution_as_str:
+#         if nb >0:
+#             var_name = d2[nb]
+#             if 'z' not in var_name:
+#                 i = int(var_name.split('_')[-2])
+#                 j = int(var_name.split('_')[-1])
+#                 solution_grid[i-1][j-1] = 1
+#         elif nb <0:
+#             var_name = d2[-nb]
+#             if 'z' not in var_name:
+#                 i = int(var_name.split('_')[-2])
+#                 j = int(var_name.split('_')[-1])
+#                 solution_grid[i-1][j-1] = 0
+
+#     for nb in solution_as_str:
+#         if nb >0:
+#             var_name = d2[nb]
+#             if 'z' in var_name:
+#                 i = int(var_name.split('_')[-4])
+#                 j = int(var_name.split('_')[-3])
+#                 i_prime = int(var_name.split('_')[-2])
+#                 j_prime = int(var_name.split('_')[-1])
+#                 if solution_grid[i-1][j-1] != None:
+#                     solution_grid[i_prime-1][j_prime-1] = 1 - solution_grid[i-1][j-1]
+#                 solution_grid[i-1][j-1] = 1
+#         elif nb <0:
+#             var_name = d2[-nb]
+#             if 'z' not in var_name:
+#                 i = int(var_name.split('_')[-2])
+#                 j = int(var_name.split('_')[-1])
+#                 solution_grid[i-1][j-1] = 0
+
+ 
+    
+#     return solution_grid
+
+
+
+
 def dimacs_string_from_constraints(constraints):
-    """Numbering rule: index in list of var_name"""
+    """Numbering rule: 1 000 000 * i + 10 000 * j + 100 * i_p + j_p"""
     res = ""
     nb_var = N*N
     nb_clauses = len(constraints)
     res+= f"p cnf {nb_var} {nb_clauses}\n"
 
-    L = []
     for constraint in constraints:
         for var_name in constraint:
-            if var_name not in L:
-                L.append(var_name) 
-
-    sorted_L = sorted(L)
-    d = dict()      # keys of d = variable names and their values are indexes
-    d2 = dict()     # keys of d2 = indexes and their values are variable names
-
-    for i,v in enumerate(L):
-        d[v] = i+1
-        d2[i+1] = v
-
-    for constraint in constraints:
-        for var_name in constraint:
-            index = d[var_name]
-            res+= str(index) + " " 
+            if 'z' not in var_name:
+                i_p = 0
+                j_p = 0
+                i = int(var_name.split('_')[-2])
+                j = int(var_name.split('_')[-1])
+            if 'z' in var_name:
+                i_p = i
+                j_p = j
+                i = int(var_name.split('_')[-4])
+                j = int(var_name.split('_')[-3]) 
+            nb = 1000000 * i + 10000 * j + 100 * i_p + j_p
+            if '-' in var_name:
+                nb = nb * (-1)
+            res+= str(nb) + " " 
         res+= "0\n"
+    print(res)
+    return res
 
-    return res, d2
-
-
-def solution_from_dimacs_string(dimacs_str, d2):
+    
+def solution_from_dimacs_string(dimacs_str):
     # write the dimacs into a file
     with open("constraints.dimacs.tmp", 'w') as f:
         f.write(dimacs_str)
@@ -166,6 +260,7 @@ def solution_from_dimacs_string(dimacs_str, d2):
     subprocess.run(["minisat", "constraints.dimacs.tmp", "out.tmp"])
     with open("out.tmp", 'r') as output:
         solution_as_str = output.read().splitlines()
+
     os.remove("constraints.dimacs.tmp")
     os.remove("out.tmp")
 
@@ -173,19 +268,19 @@ def solution_from_dimacs_string(dimacs_str, d2):
         return "UNSAT"
     # else
     solution_as_str = {int(i) for i in solution_as_str[1].split()}
-    solution_grid = [[0 for _ in range(N)] for __ in range(N)]
-    for nb in solution_as_str:
-        if nb >0:
-            var_name = d2[nb]
-            if 'z' not in var_name:
-                i = int(var_name.split('_')[-2])
-                j = int(var_name.split('_')[-1])
-            # if 'z' in var_name:
-            #     i_prime = int(var_name.split('_')[-4])
-            #     j_prime = int(var_name.split('_')[-3])
-                solution_grid[i-1][j-1] = 1
-    
+    solution_grid = [[None for _ in range(N)] for __ in range(N)]
+    for i in range(1,N):
+        for j in range(1,N):
+            for i_p in range(1, N):
+                for j_p in range(1, N):
+                    if 1000000 * i + 10000 * j + 100 * i_p + j_p in solution_as_str:
+                       # z thing
+                       None
+                    if 1000000 * i + 10000 * j in solution_as_str:
+                        solution_grid[i-1][j-1] = 1
+                    if -1000000 * i + 10000 * j + 100 * i_p + j_p in solution_as_str:
+                       # z thing
+                       None
+                    if -1000000 * i + 10000 * j in solution_as_str:
+                        solution_grid[i-1][j-1] = 0
     return solution_grid
-
-
-
